7f15e9be8cf086a59da96e8e469c65b37d8f8c9a
Its-Claire <rizkicahya.crimson03@gmail.com>
2025-08-13 02:50:40 +0700

    Feat: Delete Icon & Change Title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,6 @@
     <header class="bg-white shadow-md">
         <div class="container mx-auto flex justify-between items-center py-5 px-6">
             <div class="flex items-center gap-3">
-                <img src="./assets/vue.svg" alt="Company Logo" class="h-10 w-10" />
                 <span class="text-2xl font-bold text-gray-800 tracking-wide">Fifty-Six.</span>
             </div>
             <nav class="space-x-8 hidden md:block">
